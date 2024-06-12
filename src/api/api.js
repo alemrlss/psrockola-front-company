e60b3b5d040693d381cb139dd724de0f3f49d08f
unknown <Alejandro>
2024-06-12 22:26:58 +0300

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,27 @@
-// api.js
 import axios from "axios";
 
-// develop: http://localhost:5000/api
-// production: https://psrockola-backend-develop.onrender.com/api
+// Crear instancia de Axios
 const api = axios.create({
   baseURL: "https://psrockola-backend-develop.onrender.com/api", // Reemplaza con la URL de tu servidor NestJS
   headers: {
     "Content-Type": "application/json",
-    // Puedes agregar otros encabezados aquí según tus necesidades
   },
 });
+
+// Interceptor para agregar el token en cada solicitud
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // Obtén el token del localStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 
 export default api;
